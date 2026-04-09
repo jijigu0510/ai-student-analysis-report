d7615aab99ecc1524fc2082922e2c45f7c92044c
jijigu0510 <dddddddd9487@gmail.com>
2026-04-09 20:50:17 +0900

    Delete scan_quotes.ps1

--- scan_quotes.ps1
+++
@@ -1,8 +0,0 @@
-$lines = Get-Content "app.js" -Encoding UTF8
-for ($i = 0; $i -lt $lines.Length; $i++) {
-    $line = $lines[$i]
-    $quotes = $line.Split('"').Length - 1
-    if ($quotes % 2 -ne 0) {
-        Write-Output "Line $($i+1): $line"
-    }
-}
